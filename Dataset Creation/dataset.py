bf259866b9b7f720ed0fed5320d27819a6c18056
LaiaMarinello <laia.marinello01@estudiant.upf.edu>
2024-05-24 12:14:34 +0200

    datasep.py  main.py

--- Dataset Creation/dataset.py
+++ Dataset Creation/dataset.py
@@ -1,5 +1,9 @@
 import mirdata
 import pandas as pd
+import librosa 
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import Audio
 
 def initDataset(path):
     """
@@ -38,5 +42,64 @@ def get_random_song():
     song = saraga.choice_track()
     return song
 
-def laia():
-    return 
+def load_mixed_audio(track_id):
+    """
+    For <track_id>, return the loaded audio
+    """
+    audio_path = saraga.track(track_id).audio_path
+    audio_array, sr = librosa.load(audio_path, sr=44100)    
+    return audio_array
+
+def load_violin_audio(track_id):
+    """
+    For <track_id>, return the isolated violin track
+    """
+    audio_path = saraga.track(track_id).audio_violin_path
+    audio_array, sr = librosa.load(audio_path, sr=44100)
+    return audio_array
+
+def load_voice_audio(track_id):
+    """
+    For <track_id>, return the isolated voice track
+    """
+    audio_path = saraga.track(track_id).audio_vocal_path
+    if audio_path != None:
+        audio_array, sr = librosa.load(audio_path, sr=44100)    
+        return audio_array
+    else:
+        return None
+
+def load_mridangam_audio(track_id):
+    """
+    For <track_id>, return the isolated mridangam track
+    """
+    audio_path_right = saraga.track(track_id).audio_mridangam_right_path
+    audio_path_left = saraga.track(track_id).audio_mridangam_left_path
+
+    audio_array_right, sr = librosa.load(audio_path_right, sr=44100,mono=False)    
+    audio_array_left, sr = librosa.load(audio_path_left, sr=44100,mono=False)
+    audio_array = librosa.to_mono(np.array([audio_path_left,audio_path_right]))
+    return audio_array
+
+def plot_waveform(audio_array,amplitude="normal"):
+    """
+    Plot waveform for <audio_array> using matplotlib.pyplot
+    """
+    
+    if amplitude=="dB":
+        audio_array = 20*np.log10(audio_array)
+    plt.figure().set_figwidth(20)
+    minutes = np.arange(len(audio_array))/(44100*60)
+    # plt.magnitude_spectrum(audio_array, Fs=44100, scale='dB', color='C1')
+    plt.plot(minutes,audio_array)
+    plt.xlabel('Minutes')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+
+def play_audio(audio_array):
+    """
+    Generate audio player for <audio_array> using Ipython library
+    """
+    display(Audio(audio_array, rate=44100))
+
--- Dataset Creation/main.py
+++ Dataset Creation/main.py
@@ -1,4 +1,10 @@
 import dataset
+import librosa
+
 dataset.initDataset(r"C:\Users\w190973\OneDrive - Worldline SA\Escritorio\mtg\dataset")
 song = dataset.get_random_song()
 print(dataset.get_tonic(song.track_id))
+
+audio_path = r"C:\Users\w190973\OneDrive - Worldline SA\Escritorio\mtg\dataset"
+audio_array, sr = librosa.load(audio_path, sr=44100)
+print(dataset.play_audio(audio_array))
